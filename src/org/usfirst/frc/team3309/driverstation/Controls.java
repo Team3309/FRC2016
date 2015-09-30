9de38f95e09d65f616b0b2762340f2c796943da8
Matthew Krager <metoofarmer@yahoo.com>
2015-09-29 17:20:39 -0700

    JavaDoc

--- src/org/usfirst/frc/team3309/driverstation/Controls.java
+++ src/org/usfirst/frc/team3309/driverstation/Controls.java
@@ -1,5 +1,11 @@
 package org.usfirst.frc.team3309.driverstation;
 
+/**
+ * Controls that are gotten from the driverstation
+ * 
+ * @author TheMkrage
+ *
+ */
 public class Controls {
 	// All Controllers being used
 	public static XboxController driverController = new XboxController(0);
--- src/org/usfirst/frc/team3309/robot/Sensors.java
+++ src/org/usfirst/frc/team3309/robot/Sensors.java
@@ -3,6 +3,11 @@ package org.usfirst.frc.team3309.robot;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Gyro;
 
+/**
+ * All the sensors on the robot
+ * @author TheMkrage
+ *
+ */
 public class Sensors {
 	public Gyro gyro = new Gyro(0);
 	public Encoder leftDrive = new Encoder(0, 1, false);
